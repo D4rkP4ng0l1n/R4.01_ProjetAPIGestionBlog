c5b43179d7898a686c6f21cd51346621352df0d0
Saskue <clem.rieuneau@gmail.com>
2023-03-23 09:22:09 +0100

    Ajout de la gestion des autorisations

--- APIBlog.php
+++ APIBlog.php
@@ -19,99 +19,247 @@
     /// Identification du token envoyé par le client
     $bearer_token = get_bearer_token();
 
+	// Récupération du role de l'utilisateur si il est connecté
+	if(is_jwt_valid($bearer_token)) { 
+		$authorization = getAuthorization($bearer_token); 
+	} else {
+		$authorization = "anonymous";
+	}
+
 	switch ($http_method){
 		/// Cas de la méthode GET
 		case "GET" :
 			//Cas avec un id renseigné dans la requête
 			if (!empty($_GET['id'])) {
-				if (!ctype_digit($_GET['id'])) {
-					deliver_response(400, "Id reçu incorrect", NULL);
+				if($authorization == 'anonymous') {
+					deliver_response(403, "Operation non autorisée pour un utilisateur anonyme", NULL, $authorization);
+				} else {
+					if (!ctype_digit($_GET['id'])) {
+						deliver_response(400, "Id reçu incorrect", NULL, $authorization);
+					}
+					$req = $linkpdo->prepare('SELECT article.*, evaluer.evaluation, evaluer.nom FROM article LEFT JOIN evaluer ON article.IdArticle = evaluer.IdArticle WHERE article.IdArticle = :IdArticle');
+					$req->execute(array('IdArticle' => $_GET['id']));
+					$matchingData = $req->fetchAll();
+					if (empty($matchingData)) {
+						deliver_response(404, "Article non trouvé", NULL, $authorization);
+					}
 				}
-				$req = $linkpdo->prepare('SELECT * FROM article WHERE idArticle = :idArticle');
-				$req->execute(array('idArticle' => $_GET['id']));
-				$matchingData = $req->fetchAll();
-				if (empty($matchingData)) {
-	           		deliver_response(404, "Article non trouvé", NULL);
-	        	}
 	        //cas par défaut
 			} else {
-				$req = $linkpdo->prepare('SELECT * FROM article');
+				if($authorization != 'anonymous') {
+					$req = $linkpdo->prepare('SELECT article.*, evaluer.evaluation, evaluer.nom FROM article LEFT JOIN evaluer ON article.IdArticle = evaluer.IdArticle ORDER BY article.datePublication');
+				} else {
+					$req = $linkpdo->prepare('SELECT auteur, contenu, datePublication FROM article');
+				}
 				$req->execute();
 				$matchingData = $req->fetchAll();
+				/// Envoi de la réponse au Client
+				deliver_response(200, "Données reçues", $matchingData, $authorization);
 			}
-            /// Envoi de la réponse au Client
-            deliver_response(200, "Données reçues", $matchingData);
             break;
 		/// Cas de la méthode POST
 		case "POST" :
-			/// Récupération des données envoyées par le Client
+			if($authorization == 'publisher') {
+				/// Récupération des données envoyées par le Client
 			$postedData = file_get_contents('php://input');
 			$data = json_decode($postedData);
 			/// Traitement
-			if (empty($data->contenu) || empty($data->auteur)) {
-	        	deliver_response(400, "Données reçues incomplètes", NULL);
-	    	} else {
-                $req = $linkpdo->prepare('INSERT INTO article (auteur, contenu) VALUES (:auteur, :contenu)');
-			    $req->execute(array('auteur' => $data->auteur, 'contenu' => $data->contenu));
+			//cas ajout article
+			if(isset($data->contenu) && isset($data->auteur)) {
+				if (empty($data->contenu) || empty($data->auteur)) {
+		        	deliver_response(400, "Données reçues incomplètes", NULL);
+		    	} else {
+	                $req = $linkpdo->prepare('INSERT INTO article (auteur, contenu) VALUES (:auteur, :contenu)');
+				    $req->execute(array('auteur' => $data->auteur, 'contenu' => $data->contenu));
 
-			    $req = $linkpdo->prepare('SELECT * FROM article');
-			    $req->execute(array());
-			    $matchingData = $req->fetchAll();
+				    $req = $linkpdo->prepare('SELECT * FROM article');
+				    $req->execute(array());
+				    $matchingData = $req->fetchAll();
 
-			    /// Envoi de la réponse au Client
-			    deliver_response(201, "Article ajoutée", $matchingData);
-            }
-			break;
+				    /// Envoi de la réponse au Client
+				    deliver_response(201, "Article ajoutée", $matchingData);
+	            }
+	        //cas like/dislike article
+			} else {
+				if (empty($data->IdArticle) || empty($data->auteur) || empty($data->eval)) {
+		        	deliver_response(400, "Données reçues incomplètes", NULL, $authorization);
+		    	} else {
+		    		$req = $linkpdo->prepare('SELECT * FROM evaluer WHERE IdArticle = :IdArticle AND nom = :nom');
+					$req->execute(array('IdArticle' => $data->IdArticle, 'nom' => $data->auteur));
+
+		    		//cas like
+		    		if($data->eval == "Like") {
+					    //Ajout du like si l'article n'avait pas été liké ou disliké
+					    if($req->rowCount() == 0) {
+					    	//Nous avons décidés de faire un update dans un POST pour éviter au client de devoir accéder à la base de données
+					    	$req = $linkpdo->prepare('INSERT INTO evaluer (evaluation, nom, IdArticle) VALUES (:evaluation, :nom, :IdArticle)');
+					    	$req->execute(array('evaluation' => 1, 'nom' => $data->auteur, 'IdArticle' => $data->IdArticle));
+					    } else {
+					    	$result = $req->fetch();
+					    	$result['evaluation'];
+					    	//cas où on change le dislike en like
+					    	if($result['evaluation'] == -1) {
+					    		$req = $linkpdo->prepare('UPDATE evaluer SET evaluation = :evaluation WHERE IdArticle = :IdArticle AND nom = :nom');
+                				$req->execute(array('evaluation' => 1, 'IdArticle' => $data->IdArticle, 'nom' => $data->auteur));
+                			//cas où l'utilisateur retire son like
+					    	} else {
+					    		//même raisonement que pour le insert plus haut
+					    		$req = $linkpdo->prepare('DELETE FROM evaluer WHERE IdArticle = :IdArticle  AND nom = :nom');
+                				$req->execute(array('IdArticle' => $data->IdArticle, 'nom' => $data->auteur));
+					    	}
+					    }
+		    		}
+		    		
+		    		//cas dislike
+		    		if($data->eval == "Dislike") {
+		    			//Ajout du dislike si l'article n'avait pas été liké ou disliké
+					    if($req->rowCount() == 0) {
+					    	//Nous avons décidés de faire un update dans un POST pour éviter au client de devoir accéder à la base de données
+					    	$req = $linkpdo->prepare('INSERT INTO evaluer (evaluation, nom, IdArticle) VALUES (:evaluation, :nom, :IdArticle)');
+					    	$req->execute(array('evaluation' => -1, 'nom' => $data->auteur, 'IdArticle' => $data->IdArticle));
+					    } else {
+					    	$result = $req->fetch();
+					    	$result['evaluation'];
+					    	//cas où on change le like en dislike
+					    	if($result['evaluation'] == 1) {
+					    		$req = $linkpdo->prepare('UPDATE evaluer SET evaluation = :evaluation WHERE IdArticle = :IdArticle AND nom = :nom');
+                				$req->execute(array('evaluation' => -1, 'IdArticle' => $data->IdArticle, 'nom' => $data->auteur));
+                			//cas où l'utilisateur retire son like
+					    	} else {
+					    		//même raisonement que pour le insert plus haut
+					    		$req = $linkpdo->prepare('DELETE FROM evaluer WHERE IdArticle = :IdArticle  AND nom = :nom');
+                				$req->execute(array('IdArticle' => $data->IdArticle, 'nom' => $data->auteur));
+					    	}
+					    }
+		    		}
+
+				    $req = $linkpdo->prepare('SELECT * FROM evaluer');
+				    $req->execute(array());
+				    $matchingData = $req->fetchAll();
+
+				    /// Envoi de la réponse au Client
+				    deliver_response(201, "Evaluation ajouté", $matchingData, $authorization);
+		    	}
+			}
+		}
+		break;
 		/// Cas de la méthode PUT
 		case "PUT" :
-			/// Récupération des données envoyées par le Client
-			$postedData = file_get_contents('php://input');
-			$data = json_decode($postedData);
-			if (empty($data->contenu) || empty($_GET['id'])) {
-	        	deliver_response(400, "Données incomplètes", NULL);
-	    	} else {
-                /// Traitement
-                $req = $linkpdo->prepare('UPDATE article SET contenu = :contenu WHERE idArticle = :idArticle');
-                $req->execute(array('contenu' => $data->contenu, 'idArticle' => $_GET['id']));
+			if ($authorization == 'publisher') {
+				/// Récupération des données envoyées par le Client
+				$postedData = file_get_contents('php://input');
+				$data = json_decode($postedData);
+				if (empty($data->contenu) || empty($_GET['id'])) {
+					deliver_response(400, "Données incomplètes", NULL, $authorization);
+				} else {
+					$nom = getNom($bearer_token);
+					$verifOK = 0;
+					$req = $linkpdo->prepare('SELECT count(*) FROM article WHERE IdArticle = :IdArticle AND auteur = :auteur');
+					$req->execute(array('IdArticle' => $_GET['id'], 'auteur' => $nom));
+					$result = $req->fetch();
+					$verifOK = $result[0];
+					if($verifOK > 0) {
+						/// Traitement
+						$req = $linkpdo->prepare('UPDATE article SET contenu = :contenu WHERE idArticle = :idArticle');
+						$req->execute(array('contenu' => $data->contenu, 'idArticle' => $_GET['id']));
 
-                $req = $linkpdo->prepare('SELECT * FROM article');
-                $req->execute(array());
-                $matchingData = $req->fetchAll();
+						$req = $linkpdo->prepare('SELECT * FROM article');
+						$req->execute(array());
+						$matchingData = $req->fetchAll();
 
-                /// Envoi de la réponse au Client
-                deliver_response(200, "Article modifié", $matchingData);
-            }
+						/// Envoi de la réponse au Client
+						deliver_response(200, "Article modifié", $matchingData, $authorization);
+					}
+				}
+			} else {
+				deliver_response(403, "Operation non autorisée pour un utilisateur non publisher", NULL, $authorization);
+			}
 			break;
 		/// Cas de la méthode DELETE
 		case "DELETE" :
-			/// Récupération de l'identifiant de la ressource envoyé par le Client
-			if (!empty($_GET['id'])){
-				$req = $linkpdo->prepare('DELETE FROM article WHERE idArticle = :idArticle');
-				$req->execute(array('idArticle' => $_GET['id']));
-                if($req->rowCount() > 0) {
-                    deliver_response(200, "Article supprimé", NULL);
-                } else {
-                    deliver_response(404, "Article innexistant", NULL);
-                }
-                /// Envoi de la réponse au Client
-			} else {
-
-            }
+				/// Récupération de l'identifiant de la ressource envoyé par le Client
+				if ($authorization != 'anonymous') {
+					if (!empty($_GET['id'])) {
+						$nom = getNom($bearer_token);
+						$verifOK = 0;
+						if($authorization == 'publisher') {
+							$req = $linkpdo->prepare('SELECT count(*) FROM article WHERE IdArticle = :IdArticle AND auteur = :auteur');
+							$req->execute(array('IdArticle' => $_GET['id'], 'auteur' => $nom));
+							$result = $req->fetch();
+							$verifOK = $result[0];
+						}
+						if($authorization == 'moderator' || $verifOK > 0) {
+							// Suppression des likes/dislikes
+							$req = $linkpdo->prepare('DELETE FROM evaluer WHERE IdArticle = :IdArticle');
+							$req->execute(array('IdArticle' => $_GET['id']));
+	
+							// Suppression de l'article
+							$req = $linkpdo->prepare('DELETE FROM article WHERE IdArticle = :IdArticle');
+							$req->execute(array('IdArticle' => $_GET['id']));
+							if($req->rowCount() > 0) {
+								deliver_response(200, "Article supprimé", NULL, $authorization);
+							} else {
+								deliver_response(404, "Article innexistant", NULL, $authorization);
+							}
+						} else {
+							$req = $linkpdo->prepare('SELECT count(*) FROM article WHERE IdArticle = :IdArticle');
+							$req->execute(array('IdArticle' => $_GET['id']));
+							if($req->rowCount() >= 1) {
+								deliver_response(403, "Operation non autorisée, ce n'est pas votre article", NULL, $authorization);
+							} else {
+								deliver_response(404, "Article innexistant", NULL, $authorization);
+							}
+						}
+					}
+					else {
+						deliver_response(400, "Données reçues incomplètes", NULL, $authorization);
+					}
+				} else {
+					deliver_response(403, "Operation non autorisée pour un utilisateur anonyme", NULL, $authorization);
+				}
             break;
 		default :
-			deliver_response(405, "Cette requête n'est pas implémentée dans l'API", NULL);
+			deliver_response(405, "Cette requête n'est pas implémentée dans l'API", NULL, $authorization);
 		}
 
 	/// Envoi de la réponse au Client
-	function deliver_response($status, $status_message, $data){
+	function deliver_response($status, $status_message, $data, $authorization){
 		/// Paramétrage de l'entête HTTP, suite
 		header("HTTP/1.1 $status $status_message");
 		/// Paramétrage de la réponse retournée
 		$response['status'] = $status;
 		$response['status_message'] = $status_message;
 		$response['data'] = $data;
+		$response['authorization'] = $authorization;
 		/// Mapping de la réponse au format JSON
 		$json_response = json_encode($response);
 		echo $json_response;
 	}
+
+	/// Fonction pour décoder un token JWT
+	function decodeJWT($token) {
+		$token_parts = explode('.', $token);
+		$decoded_claims = base64_decode($token_parts[1]);
+		$claims = json_decode($decoded_claims, true);
+		return $claims;
+	}
+
+	/// Fonction pour récupérer le role d'un utilisateur
+	function getAuthorization($token) {
+		if(is_jwt_valid($token)) {
+			$decoded_token = decodeJWT($token);
+			$authorization = $decoded_token['role'];
+		} else {
+			$authorization = 'anonymous';
+		}
+		return $authorization;
+	}
+
+	function getNom($token) {
+		if(is_jwt_valid($token)) {
+			$decoded_token = decodeJWT($token);
+			return $decoded_token['user'];
+		}
+	}
+
 ?>
--- index.php
+++ index.php
@@ -13,18 +13,19 @@
 
             // Démarage de la session et vérification de la validité du jwt s'il est stocké
             session_start();
-            //unset($_SESSION['jwt']);
-            //unset($_SESSION['user']);
+
+            // unset($_SESSION['jwt']);
+            // unset($_SESSION['user']);
             if(isset($_SESSION['jwt'])) {
-                echo $_SESSION['jwt'];
-                echo $_SESSION['user'];
+                // echo $_SESSION['jwt'];
+                // echo $_SESSION['user'];
             }
             
 			// Execution après validation du form d'ajout d'article
 			if (isset($_POST['contenuNewArticle'])) {
 				////////////////// Cas de la méthode POST //////////////////
 				/// Déclaration des données à envoyer au Serveur
-				$data = array("auteur" => "Jules", "contenu" => $_POST['contenuNewArticle']); //MODIFIER AUTEUR PAR LE NOM DE COMPTE
+				$data = array("auteur" => $_SESSION['user'], "contenu" => $_POST['contenuNewArticle']); //MODIFIER AUTEUR PAR LE NOM DE COMPTE
 				$data_string = json_encode($data);
 				/// Envoi de la requête
 				$result = file_get_contents($api,null,stream_context_create(array('http' => array('method' => 'POST', 'content' => $data_string,'header' => array('Content-Type: application/json'."\r\n".'Content-Length: '.strlen($data_string)."\r\n".'Authorization: Bearer '.$_SESSION['jwt'])))));
@@ -49,21 +50,46 @@
 				header('Location: index.php');
 			}
 
+			// Execution après clic sur le bouton liker
+			if (isset($_GET['likeId'])) {
+				////////////////// Cas de la méthode POST //////////////////
+				/// Déclaration des données à envoyer au Serveur
+				$data = array("auteur" => $_SESSION['user'], "IdArticle" => $_GET['likeId'], "eval" => "Like");
+				$data_string = json_encode($data);
+				/// Envoi de la requête
+				$result = file_get_contents($api,null,stream_context_create(array('http' => array('method' => 'POST', 'content' => $data_string,'header' => array('Content-Type: application/json'."\r\n".'Content-Length: '.strlen($data_string)."\r\n".'Authorization: Bearer '.$_SESSION['jwt'])))));
+				header('Location: index.php');
+			}
+			
+			// Execution après clic sur le bouton disliker
+			if (isset($_GET['dislikeId'])) {
+				////////////////// Cas de la méthode POST //////////////////
+				/// Déclaration des données à envoyer au Serveur
+				$data = array("auteur" => $_SESSION['user'], "IdArticle" => $_GET['dislikeId'], "eval" => "Dislike");
+				$data_string = json_encode($data);
+				/// Envoi de la requête
+				$result = file_get_contents($api,null,stream_context_create(array('http' => array('method' => 'POST', 'content' => $data_string,'header' => array('Content-Type: application/json'."\r\n".'Content-Length: '.strlen($data_string)."\r\n".'Authorization: Bearer '.$_SESSION['jwt'])))));
+				header('Location: index.php');
+			}
+
 			// Chargement de la page d'un utilisateur après le clic sur le bouton se connecter
             if (isset($_POST['connectionButton'])) {
                 if(!empty($_POST['user']) && !empty($_POST['pwd'])) {
                     $data = array('user' => $_POST['user'], 'pwd' => $_POST['pwd']);
                     $dataEncode = json_encode($data);
-                    $result = file_get_contents($apiAuth, null, stream_context_create(array('http' => array('method' => 'POST', 'content' => $dataEncode, 'header' => array('Content-Type: application/json'."\r\n".'Content-Length: '.strlen($dataEncode)."\r\n".'Authorization: Bearer '.$_SESSION['jwt'])))));
-                    $decodeResult = json_decode($result);
-                    //print_r($decodeResult);
-                       $_SESSION['jwt'] = $decodeResult->token;
-                       if(isset($_SESSION['jwt'])) {
-                           $_SESSION['user'] = $_POST['user'];
-                       }
-                       header('Location: index.php');
+                    $result = file_get_contents($apiAuth, null, stream_context_create(array('http' => array('ignore_errors' => true, 'method' => 'POST', 'content' => $dataEncode, 'header' => array('Content-Type: application/json'."\r\n".'Content-Length: '.strlen($dataEncode)."\r\n".'Authorization: Bearer '.$_SESSION['jwt'])))));
+					$decodeResult = json_decode($result);
+					if($decodeResult->status == 404) {
+						echo "Nom d'utilisateur ou mot de passe incorrect";
+					} else {
+						$_SESSION['jwt'] = $decodeResult->token;
+						if(isset($_SESSION['jwt'])) {
+							$_SESSION['user'] = $_POST['user'];
+						}
+						//header('Location: index.php');
+					}
                 } else {
-                    echo "Nom d'utilisateur ou mot de passe incorrect";
+                    echo "Nom d'utilisateur ou mot de passe vide";
                 }
             }
 
@@ -74,46 +100,98 @@
 				$result = json_decode($result, true);
 
 				foreach ($result['data'] as $res) {
-					echo "<h5>Modifier l'article:</h5>";
+					echo "<h5> Modifier l'article : </h5>";
 					echo "<form method='post' action='index.php'>";
-					echo "<textarea name='modif'>".htmlspecialchars($res['contenu'])."</textarea>";
-					echo "<input type='hidden' name='id' value='".$_GET['putId']."'>";
-					echo "<input type='submit' value='Valider'>";
+					    echo "<textarea name='modif'>".htmlspecialchars($res['contenu'])."</textarea>";
+					    echo "<input type='hidden' name='id' value='".$_GET['putId']."'>";
+					    echo "<input type='submit' value='Valider'>";
 					echo "</form>";
 				}
 			//chargement de la page par défaut
 			} else {
 				//affichage du form de connexion
 				echo "<form method='post' action='index.php'>";
-				echo "Utilisateur <input type='text' name='user'>";
-				echo "Mot de passe <input type='password' name='pwd'>";
-				echo "<input type='submit' name='connectionButton' value='Se connecter'>";
+				    echo "Utilisateur <input type='text' name='user'> </br>";
+				    echo "Mot de passe <input type='password' name='pwd'> </br>";
+				    echo "<input type='submit' name='connectionButton' value='Se connecter'>";
 				echo "</form>";
 
 				//affichage du form d'ajout d'article
-				echo "<h5>Ajouter un article :</h5>";
+				echo "<h5> Ajouter un article : </h5>";
 				echo "<form method='post' action='index.php'>";
-				echo "<textarea name='contenuNewArticle' value=''> </textarea>" ;
-				echo "<input type='submit' value='Valider'>";
+				    echo "<textarea name='contenuNewArticle' value=''> </textarea>" ;
+				    echo "<input type='submit' value='Valider'>";
 				echo "</form>";
 
 				//affichage des articles
 				////////////////// Cas de la méthode GET //////////////////
-				$result = file_get_contents($api,false,stream_context_create(array('http' => array('method' => 'GET', 'header' => 'Authorization: Bearer '.$_SESSION['jwt']))));
-				$result = json_decode($result, true);
-				foreach ($result['data'] as $res) {
-						echo "<table>";
-						echo "<tbody>";
-						echo "<tr>";
-						echo "<td>".htmlspecialchars($res['auteur'])."</td>";
-						echo "<td>".htmlspecialchars($res['contenu'])."</td>";
-						$id = $res['IdArticle'];
-						echo "<td><a href='index.php?putId=$id'>Modifier</a></td>";
-						echo "<td><a href='index.php?deleteId=$id'>Supprimer</a></td>";
-						echo "<tr>";
-						echo "</tbody>";
-						echo "</table>";
+				if(isset($_SESSION['jwt'])) {
+					$result = file_get_contents($api, false, stream_context_create(array('http' => array('method' => 'GET', 'header' => 'Authorization: Bearer '.$_SESSION['jwt']))));
+				} else {
+					$result = file_get_contents($api, false, stream_context_create(array('http' => array('method' => 'GET'))));
 				}
+				$result = json_decode($result, true);
+				$alreadyDisplayed = array(); // Tableau pour stocker les ID déjà affichés
+
+				echo "<table>";
+				echo "<tbody>";
+				foreach ($result['data'] as $res) {
+					$id = $res['IdArticle'];
+    				if (!in_array($id, $alreadyDisplayed)) { // Si l'ID n'a pas déjà été affiché
+    					// Tableaux pour stocker les utilisateur qui ont liké/disliké
+    					$likes = array();
+				        $dislikes = array();
+				        foreach ($result['data'] as $res2) {
+				            if ($res2['IdArticle'] == $id) {
+				                if ($res2['evaluation'] == 1) {
+				                    $likes[] = $res2['nom'];
+				                } else if ($res2['evaluation'] == -1) {
+				                    $dislikes[] = $res2['nom'];
+				                }
+				            }
+				        }
+						echo "<tr>";
+							echo "<td>".htmlspecialchars($res['auteur'])."</td>";
+							echo "<td>".htmlspecialchars($res['contenu'])."</td>";
+							$id = $res['IdArticle'];
+							echo "<td><a href='index.php?putId=$id'>Modifier</a></td>";
+							echo "<td><a href='index.php?deleteId=$id'>Supprimer</a></td>";
+							if($result['authorization'] == 'publisher') {
+								echo "<td><a href='index.php?likeId=$id'>Liker</a></td>";
+								echo "<td><a href='index.php?dislikeId=$id'>Disliker</a></td>";
+							}
+							if($result['authorization'] != 'anonymous') {
+								if (!empty($likes)) {
+									$first = true;
+									foreach ($likes as $like) {
+										if (!$first) {
+											echo ", ";
+										}
+										echo htmlspecialchars($like);
+										$first = false;
+									}
+								}
+								echo "</td>";
+								echo "<td>Dislikes : ".count($dislikes)."<br>";
+								if (!empty($dislikes)) {
+									$first = true;
+									foreach ($dislikes as $dislike) {
+										if (!$first) {
+											echo ", ";
+										}
+										echo htmlspecialchars($dislike);
+										$first = false;
+									}
+							}
+							echo "<td>Likes : ".count($likes)."<br>";
+				        }
+				        echo "</td>";
+						echo "</tr>";
+						$alreadyDisplayed[] = $id; // ajouter l'ID à la liste des articles déjà affichés
+					}
+				}
+				echo "</tbody>";
+				echo "</table>";
 			}
 		?>
 	</body>
